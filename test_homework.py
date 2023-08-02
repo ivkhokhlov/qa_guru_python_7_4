0178c2aa5b49e7f4ba187a9f02ba75d0d1fbedde
Ivan Khokhlov <xxx.mailbox73@gmail.com>
2023-08-02 20:55:59 +0400

    hw2

--- test_homework.py
+++ test_homework.py
@@ -10,6 +10,7 @@ def test_greeting():
     age = 25
     # TODO Сформируйте нужную строку
     output = f"Привет, {name}! Тебе {age} лет."
+    print(output)
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
 
@@ -39,10 +40,12 @@ def test_circle():
     r = 23
     # TODO сосчитайте площадь
     area = math.pi * pow(r, 2)
+    print(area)
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
     length = math.pi * 2 * r
+    print(length)
     assert length == 144.51326206513048
 
 
@@ -81,5 +84,6 @@ def test_dicts():
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
     d = dict(zip(first, second))
+    print(d.values())
     assert isinstance(d, dict)
     assert len(d) == 5
